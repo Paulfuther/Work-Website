439fdeb83b1fe27d2ecd6b06c835bac497f79fe3
Paul Futher <paul.futher@gmail.com>
2020-03-22 21:24:14 -0400

    db udpates

--- Flaskblog.py
+++ Flaskblog.py
@@ -58,12 +58,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
     
     def __repr__(self):
-        return f"User('{self.username}, {self.email}, {self.image}')"
+        return f"User('{self.username}', '{self.email}')"
     
     
 class Post(db.Model):  
@@ -74,7 +74,7 @@ class Post(db.Model):
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}, {self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
     
     
 posts = [
