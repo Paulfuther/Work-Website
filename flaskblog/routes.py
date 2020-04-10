d8356de11bf90fbb07957761c1e207ec9e0661bb
Paul Futher <paul.futher@gmail.com>
2020-04-10 17:34:44 -0400

    pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -24,7 +24,8 @@ chartstore = 48314
 @app.route("/")
 @app.route("/home")
 def home():
-    posts=Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page=3)
     return render_template('home.html',posts=posts)
 
 
@@ -595,3 +596,14 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your Post Has Been Deleted', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=3)
+    return render_template('user_posts.html', posts=posts, user=user)
